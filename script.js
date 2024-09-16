fe71fe678b907a0fa5ac5f917cd469298855d40d
julianasantoss <corazondemayr@gmail.com>
2024-09-16 17:02:12 -0500

    Union y arreglos finales

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Funcion Scroll Componentes de Navegacion
 function scrollToSection(id) {
     const element = document.getElementById(id);
     if (element) {
@@ -10,6 +11,7 @@ function scrollToSection(id) {
     }
 }
     
+//Funcion Carro de Compras
     const carroItems = {};
 
    function addCarrito(idItem) {
@@ -74,5 +76,17 @@ function scrollToSection(id) {
 
 
   
+/* se crea un evento al scroll, cuando este se desplace mas de 50px este
+empiezaa ponerse transparente */
+
+document.addEventListener('scroll', function () {
+    const nav = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        nav.classList.add('translucent');
+    } else {
+        nav.classList.remove('translucent');
+    }
+});
+ 
 
 
